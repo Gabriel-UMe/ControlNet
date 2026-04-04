a6a5be2a46c1d46ef8b22b28c088518414f6d474
Gabriel Hare <gabriel@universalme.io>
2026-04-04 03:49:34 +0000

    WIP training on multiple GPUs

--- tutorial_train.py
+++ tutorial_train.py
@@ -3,12 +3,41 @@ from share import *
 import json
 import os
 import pytorch_lightning as pl
+from pytorch_lightning.strategies import DDPStrategy
 import torch
 from torch.utils.data import DataLoader
 
 from tutorial_dataset import MyDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
+
+# Recommended setting in case OOM occurs.
+# https://pytorch.org/docs/stable/notes/cuda.html#environment-variables
+os.environ['PYTORCH_ALLOC_CONF'] = 'expandable_segments:True'
+
+# If the GPUs are on separate PCIe root complexes, using GLOO instead of NCCL
+# To check this run
+#     $ lspci -tv 2>/dev/null
+# Example:
+# -+-[0000:00]-+-00.0  Advanced Micro Devices, Inc. [AMD] Starship/Matisse Root Complex
+#  |           +-00.2  Advanced Micro Devices, Inc. [AMD] Milan IOMMU
+#  |           +-01.0  Advanced Micro Devices, Inc. [AMD] Starship/Matisse PCIe Dummy Host Bridge
+#  |           ...
+#  |           +-03.1-[01]--+-00.0  NVIDIA Corporation AD102 [GeForce RTX 4090]
+#  |           |            \-00.1  NVIDIA Corporation AD102 High Definition Audio Controller
+# +-[0000:40]-+-00.0  Advanced Micro Devices, Inc. [AMD] Starship/Matisse Root Complex
+#  |           +-00.2  Advanced Micro Devices, Inc. [AMD] Milan IOMMU
+#  |           +-01.0  Advanced Micro Devices, Inc. [AMD] Starship/Matisse PCIe Dummy Host Bridge
+#  |           +-01.1-[41]--+-00.0  NVIDIA Corporation AD102 [GeForce RTX 4090]
+#  |           |            \-00.1  NVIDIA Corporation AD102 High Definition Audio Controller
+# NOTE: Gloo copies gradients through CPU RAM for synchronization,
+# which is slower than NCCL with P2P.
+process_group_backend='gloo'
+# process_group_backend='nccl'
+
+# https://docs.pytorch.org/docs/main/distributed.html#debugging-torch-distributed-applications
+os.environ['TORCH_CPP_LOG_LEVEL'] = 'INFO'
+os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'DETAIL'
 
 # PROBLEM:
 # Stack trace of the failed collective not found, potentially because FlightRecorder is disabled.
@@ -17,13 +46,6 @@ from cldm.model import create_model, load_state_dict
 os.environ['TORCH_NCCL_TRACE_BUFFER_SIZE'] = '10000000'
 os.environ['NCCL_DEBUG'] = 'INFO'
 os.environ['NCCL_DEBUG_SUBSYS'] = 'ALL'
-# https://docs.pytorch.org/docs/main/distributed.html#debugging-torch-distributed-applications
-os.environ['TORCH_CPP_LOG_LEVEL'] = 'INFO'
-os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'DETAIL'
-
-# Recommended setting in case OOM occurs.
-# https://pytorch.org/docs/stable/notes/cuda.html#environment-variables
-os.environ['PYTORCH_ALLOC_CONF'] = 'expandable_segments:True'
 
 # Support use of tensor cores
 # https://docs.pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
@@ -31,20 +53,19 @@ torch.set_float32_matmul_precision('high')
 
 # Configs
 resume_path = './models/control_sd15_ini.ckpt'
-batch_size = 4
 logger_freq = 300
 learning_rate = 1e-5
 sd_locked = True
 only_mid_control = False
+# NOTE: Reduce batch_size here and increase accumulate_grad_batches proportionately if OOM occurs.
+batch_size = 2
 
-
-# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
+# First, use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
 model = create_model('./models/cldm_v15.yaml').cpu()
 model.load_state_dict(load_state_dict(resume_path, location='cpu'))
 model.learning_rate = learning_rate
 model.sd_locked = sd_locked
 model.only_mid_control = only_mid_control
-
 
 # Misc
 dataset = MyDataset()
@@ -91,20 +112,23 @@ if os.path.exists('PLTrainer.json'):
     with open('PLTrainer.json', 'r') as f:
         trainer_config = json.load(f)
     if trainer_config.get('devices', None) is None:
-        trainer_config['devices'] = select_all_cuda_devices()
+        trainer_config['devices'] = select_one_cuda_device()
         trainer_config['accelerator'] = 'gpu'
     trainer_config['callbacks'] = [logger]
     trainer = pl.Trainer(**trainer_config)
     print("Configured PyTorch-Lightning trainer from file")
 else:
-    # Works on a single 4090 GPU
-    devices = select_one_cuda_device()
+    selected_devices = select_all_cuda_devices()
     trainer = pl.Trainer(
         callbacks = [logger],
         max_epochs=1,
-        precision = 32,
-        num_nodes=1, devices=devices, accelerator='gpu',
-        accumulate_grad_batches=1, strategy='auto'
+        precision='bf16-mixed',
+        num_nodes=1, devices=selected_devices, accelerator='gpu',
+        accumulate_grad_batches=2,
+        strategy=DDPStrategy(
+            process_group_backend=process_group_backend,
+            find_unused_parameters=True,
+        ),
     )
 
 # Train!
