df5c2761a3339510590fe7ddbc218b189519bc80
Gabriel Hare <gabriel@universalme.io>
2026-04-08 00:25:29 +0000

    Fix: restore use of nccl

--- tutorial_train.py
+++ tutorial_train.py
@@ -34,16 +34,13 @@ os.environ['PYTORCH_ALLOC_CONF'] = 'expandable_segments:True'
 # which is slower than NCCL with P2P.
 process_group_backend='gloo'
 
-can_use_nccl = False
+can_use_nccl = True
 if can_use_nccl:
     process_group_backend='nccl'
-    # PROBLEM:
-    # Stack trace of the failed collective not found, potentially because FlightRecorder is disabled.
-    # You can enable it by setting TORCH_NCCL_TRACE_BUFFER_SIZE to a non-zero value.
-    # SOLUTION:
-    os.environ['TORCH_NCCL_TRACE_BUFFER_SIZE'] = '10000000'
-    os.environ['NCCL_DEBUG'] = 'INFO'
-    os.environ['NCCL_DEBUG_SUBSYS'] = 'ALL'
+    # Disable peer-to-peer
+    os.environ['NCCL_P2P_DISABLE'] = '1'
+    # Allow shared memory
+    os.environ['NCCL_SHM_DISABLE'] = '0'
 
 is_distributed = False
 if is_distributed:
@@ -51,6 +48,14 @@ if is_distributed:
     # https://docs.pytorch.org/docs/main/distributed.html#debugging-torch-distributed-applications
     os.environ['TORCH_CPP_LOG_LEVEL'] = 'INFO'
     os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'DETAIL'
+    if can_use_nccl:
+        # PROBLEM:
+        # Stack trace of the failed collective not found, potentially because FlightRecorder is disabled.
+        # You can enable it by setting TORCH_NCCL_TRACE_BUFFER_SIZE to a non-zero value.
+        # SOLUTION:
+        os.environ['TORCH_NCCL_TRACE_BUFFER_SIZE'] = '10000000'
+        os.environ['NCCL_DEBUG'] = 'INFO'
+        os.environ['NCCL_DEBUG_SUBSYS'] = 'ALL'
 
 # Support use of tensor cores
 # https://docs.pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
