2c69973cf3ec804939b6ab5ec2c9ab7c930933e2
Gabriel Hare <gabriel@universalme.io>
2026-04-08 01:31:33 +0000

    Support multiple training configurations in files

--- tutorial_train.py
+++ tutorial_train.py
@@ -1,5 +1,6 @@
 from share import *
 
+from typing import *
 import json
 import os
 import pytorch_lightning as pl
@@ -11,51 +12,34 @@ from tutorial_dataset import MyDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
 
-# Recommended setting in case OOM occurs.
-# https://pytorch.org/docs/stable/notes/cuda.html#environment-variables
-os.environ['PYTORCH_ALLOC_CONF'] = 'expandable_segments:True'
+config_file_name = 'config_dual.json'
 
-# If the GPUs are on separate PCIe root complexes, using GLOO instead of NCCL
-# To check this run
-#     $ lspci -tv 2>/dev/null
-# Example:
-# -+-[0000:00]-+-00.0  Advanced Micro Devices, Inc. [AMD] Starship/Matisse Root Complex
-#  |           +-00.2  Advanced Micro Devices, Inc. [AMD] Milan IOMMU
-#  |           +-01.0  Advanced Micro Devices, Inc. [AMD] Starship/Matisse PCIe Dummy Host Bridge
-#  |           ...
-#  |           +-03.1-[01]--+-00.0  NVIDIA Corporation AD102 [GeForce RTX 4090]
-#  |           |            \-00.1  NVIDIA Corporation AD102 High Definition Audio Controller
-# +-[0000:40]-+-00.0  Advanced Micro Devices, Inc. [AMD] Starship/Matisse Root Complex
-#  |           +-00.2  Advanced Micro Devices, Inc. [AMD] Milan IOMMU
-#  |           +-01.0  Advanced Micro Devices, Inc. [AMD] Starship/Matisse PCIe Dummy Host Bridge
-#  |           +-01.1-[41]--+-00.0  NVIDIA Corporation AD102 [GeForce RTX 4090]
-#  |           |            \-00.1  NVIDIA Corporation AD102 High Definition Audio Controller
-# NOTE: Gloo copies gradients through CPU RAM for synchronization,
-# which is slower than NCCL with P2P.
-process_group_backend='gloo'
+# Configure environment
+environ_config: Optional[Dict[str, str]] = None
+loader_config: Optional[Dict[str, Any]] = None
+trainer_config: Optional[Dict[str, Any]] = None
+if os.path.exists(config_file_name):
+    with open(config_file_name, 'r') as f:
+        config = json.load(f)
+        environ_config = config.get('environ', None)
+        loader_config = config.get('loader', None)
+        trainer_config = config.get('trainer', None)
 
-can_use_nccl = True
-if can_use_nccl:
-    process_group_backend='nccl'
-    # Disable peer-to-peer
-    os.environ['NCCL_P2P_DISABLE'] = '1'
-    # Allow shared memory
-    os.environ['NCCL_SHM_DISABLE'] = '0'
+if environ_config is not None:
+    for key, value in environ_config.items():
+        os.environ[key] = value
+else:
+    # Recommended setting in case OOM occurs.
+    # https://pytorch.org/docs/stable/notes/cuda.html#environment-variables
+    os.environ['PYTORCH_ALLOC_CONF'] = 'expandable_segments:True'
 
-is_distributed = False
-if is_distributed:
-    # WARNING: This will cause single-node execution using gloo to hang!!!
-    # https://docs.pytorch.org/docs/main/distributed.html#debugging-torch-distributed-applications
-    os.environ['TORCH_CPP_LOG_LEVEL'] = 'INFO'
-    os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'DETAIL'
-    if can_use_nccl:
-        # PROBLEM:
-        # Stack trace of the failed collective not found, potentially because FlightRecorder is disabled.
-        # You can enable it by setting TORCH_NCCL_TRACE_BUFFER_SIZE to a non-zero value.
-        # SOLUTION:
-        os.environ['TORCH_NCCL_TRACE_BUFFER_SIZE'] = '10000000'
-        os.environ['NCCL_DEBUG'] = 'INFO'
-        os.environ['NCCL_DEBUG_SUBSYS'] = 'ALL'
+# WARNING: TORCH_CPP_LOG_LEVEL will cause single-node execution using gloo to hang!!!
+# https://docs.pytorch.org/docs/main/distributed.html#debugging-torch-distributed-applications
+# os.environ['TORCH_CPP_LOG_LEVEL'] = 'INFO'
+# os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'DETAIL'
+# os.environ['TORCH_NCCL_TRACE_BUFFER_SIZE'] = '10000000'
+# os.environ['NCCL_DEBUG'] = 'INFO'
+# os.environ['NCCL_DEBUG_SUBSYS'] = 'ALL'
 
 # Support use of tensor cores
 # https://docs.pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
@@ -77,11 +61,14 @@ model.learning_rate = learning_rate
 model.sd_locked = sd_locked
 model.only_mid_control = only_mid_control
 
-# Misc
+# Loader
 dataset = MyDataset()
-dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
+if loader_config is not None:
+    # NOTE: Reduce batch_size here and increase accumulate_grad_batches proportionately if OOM occurs.
+    dataloader = DataLoader(dataset, **loader_config)
+else:
+    dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
-
 
 # Print available CUDA devices and select the single device with the most memory
 def select_one_cuda_device():
@@ -108,7 +95,6 @@ def select_all_cuda_devices():
         print(f"- Including CUDA Device {i}: {torch.cuda.get_device_name(i)}, memory = {device_properties.total_memory / 1024**3:.2f} GB")
     return devices
 
-
 # WARNING: Setting strategy='ddp' will result in an error:
 # RuntimeError: It looks like your LightningModule has parameters that were not used in producing the loss returned by training_step.
 # If this is intentional, you must enable the detection of unused parameters in DDP,
@@ -118,27 +104,36 @@ def select_all_cuda_devices():
 # Setting strategy='ddp_find_unused_parameters_true' works!
 
 # Optionally configure PyTorch-Lightning from a file
-if os.path.exists('PLTrainer.json'):
-    with open('PLTrainer.json', 'r') as f:
-        trainer_config = json.load(f)
+if trainer_config is not None:
     if trainer_config.get('devices', None) is None:
-        trainer_config['devices'] = select_one_cuda_device()
-        trainer_config['accelerator'] = 'gpu'
-    trainer_config['callbacks'] = [logger]
+        selected_devices = select_all_cuda_devices()
+        if len(selected_devices) > 0:
+            trainer_config['devices'] = selected_devices
+            trainer_config['accelerator'] = 'gpu'
+            trainer_config['strategy'] = DDPStrategy(
+                process_group_backend='nccl',
+                find_unused_parameters=True,
+            )
+        if trainer_config.get('strategy', None) is not None:
+            trainer_config['strategy'] = DDPStrategy(**trainer_config['strategy'])
+        if trainer_config.get('max_epochs', None) is None:
+            trainer_config['max_epochs'] = 1
+        if trainer_config.get('precision', None) is None:
+            trainer_config['precision'] = 'bf16-mixed'
+        if trainer_config.get('accumulate_grad_batches', None) is None:
+            trainer_config['accumulate_grad_batches'] = 2
+    trainer_config['callbacks'] = [logger]  # override callbacks
     trainer = pl.Trainer(**trainer_config)
     print("Configured PyTorch-Lightning trainer from file")
 else:
-    selected_devices = select_all_cuda_devices()
+    selected_devices = select_one_cuda_device()
     trainer = pl.Trainer(
         callbacks = [logger],
         max_epochs=1,
         precision='bf16-mixed',
         num_nodes=1, devices=selected_devices, accelerator='gpu',
         accumulate_grad_batches=2,
-        strategy=DDPStrategy(
-            process_group_backend=process_group_backend,
-            find_unused_parameters=True,
-        ),
+        strategy='ddp_find_unused_parameters_true',
     )
 
 # Train!
